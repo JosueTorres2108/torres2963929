f481bcc67c7a9f4f0d3e4bba5ce6148c6cb56874
JosueTorres2108 <josuett2108@gmail.com>
2023-05-08 07:15:37 -0500

    nueva actualizacion

---
+++ Bucles/listas/Moda.py
@@ -0,0 +1,62 @@
+
+import random 
+tam=random.randint(5,10) 
+lista=[random.randrange(100) for i in range(tam)] 
+suma=0 
+sumae=0 
+resta=[] 
+elevado=[] 
+mayor=0 
+menor=100000 
+print(lista) 
+print(tam) 
+for i in range(tam): 
+     for j in range(i+1,tam): 
+         if lista[i]>lista[j]: 
+             aux=lista[i] 
+             lista[i]=lista[j] 
+             lista[j]=aux 
+print("Lista en orden",lista) 
+  
+for i in lista: 
+     if i>mayor:  
+         mayor=i 
+     if i<menor and i!=0: 
+         menor=i 
+  
+if len(lista)%2==0: 
+     mediana = (lista[(tam // 2) - 1] + lista[tam // 2]) / 2 
+else:  
+     mediana=lista[(len(lista)//2)] 
+  
+for i in lista: 
+     suma+=i 
+media=(suma/tam) 
+ind=0 
+for i in lista: 
+     cont=0 
+     for f in lista: 
+         if i==f: 
+             cont+=1 
+     if cont>ind: 
+         ind=cont 
+         moda=i 
+  
+for i in lista: 
+     y=((i-media)) 
+     resta.append(y) 
+for j in resta: 
+     y=((j**2)) 
+     elevado.append(y) 
+for y in elevado: 
+     sumae+=y 
+diviv=(sumae/tam-1) 
+  
+ds=diviv**0.5 
+  
+print(f"La mediana de la lista es: {mediana}") 
+print(f"la media de la lista es: {media}") 
+print(f"La moda de la lista es: {moda}") 
+print(f"La desviacion Estandar de la lista es: {ds}" ) 
+print(f"El numero menor de la lista es: {menor}") 
+print(f"El numero mayor de la lista es: {mayor}")
---
+++ Bucles/listas/tarea2.py
@@ -0,0 +1,27 @@
+import random  
+tam= random.randint(15,20) 
+lista=[random.randrange(9) for i in range(tam)] 
+x=1 
+print(lista) 
+print(tam) 
+  
+  
+cont=0 
+posicion=[] 
+x= abs(int(input("Digita un numero de 0 a 9: "))) 
+if x in lista: 
+     print("El numero existe") 
+else: 
+     print("El numero no existe") 
+for i in lista: 
+     if i == x: 
+         cont +=1 
+         posicion.append(lista.index(i)) 
+         lista.remove(i) 
+         lista.insert(i,15) 
+if cont>=2: 
+    print(f"El numero esta repetido {cont} veces") 
+    if cont==1: 
+        print(f"El valor {x} esta en la posicion: {posicion} de la lista") 
+    else: 
+        print(f"El valor {x} esta en las posiciones: {posicion} de la lista")
